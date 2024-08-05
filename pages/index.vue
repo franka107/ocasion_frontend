f7fdc1bf68383b6c65f2de89706c9572692c61c9
cristhian gomez <cristhian.gomez.7@gmail.com>
2024-08-05 01:23:51 -0500

    init shadcdn

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Welcome to the Home Page!</h1>
+        <p>This is the content of your home page.</p>
+        <Button>Test</Button>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+/* Add your page-specific styles here */
+</style>
