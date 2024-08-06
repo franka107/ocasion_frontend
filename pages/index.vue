eb0569ee0e066dddae946b11420ad69d75c208dd
cristhian gomez <cristhian.gomez.7@gmail.com>
2024-08-06 01:05:07 -0500

    Fix: pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
 <template>
     <div>
-        <h1>Welcome to the Home Page!</h1>
-        <p>This is the content of your home page.</p>
-        <Button>Test</Button>
     </div>
 </template>
 
---
+++ pages/users/administrators.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+/* Add your page-specific styles here */
+</style>
