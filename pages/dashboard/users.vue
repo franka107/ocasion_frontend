66fbe94e30b1ca51fe5f2553b1fce0a194cbea8c
cristhian gomez <cristhian.gomez.7@gmail.com>
2024-08-06 20:18:15 -0500

    Fix: user routing

--- pages/dashboard/users.vue
+++ pages/dashboard/users.vue
@@ -1,22 +1,25 @@
 <template>
    <div class="flex-grow flex flex-col pt-6">
     <div class="flex space-x-4">
-      <NuxtLink to="/users/administrators" class="py-2 px-4 rounded-t-lg" :class="{
-        'bg-white text-[#2D5979]': isActive('/users/administrators'),
-        'bg-[#DEE6EF] text-[#2D5979]': !isActive('/users/administrators'),
-      }">
+      <NuxtLink
+        to="/dashboard/users/administrators"
+        class="py-2 px-4 rounded-t-lg"
+        :class="isActive('/dashboard/users/administrators') ? 'bg-white text-[#2D5979]': 'bg-[#DEE6EF] text-[#2D5979]'"
+      >
         Administradores
       </NuxtLink>
-      <NuxtLink to="/users/organizations" class="py-2 px-4 rounded-t-lg" :class="{
-        'bg-white text-[#2D5979]': isActive('/users/organizations'),
-        'bg-[#DEE6EF] text-[#2D5979]': !isActive('/users/organizations'),
-      }">
+      <NuxtLink
+        to="/dashboard/users/organizations"
+        class="py-2 px-4 rounded-t-lg"
+        :class="isActive('/dashboard/users/organizations') ? 'bg-white text-[#2D5979]': 'bg-[#DEE6EF] text-[#2D5979]'"
+      >
         Organizaciones
       </NuxtLink>
-      <NuxtLink to="/users/participants" class="py-2 px-4 rounded-t-lg" :class="{
-        'bg-white text-[#2D5979]': isActive('/users/participants'),
-        'bg-[#DEE6EF] text-[#2D5979]': !isActive('/users/participants'),
-      }">
+      <NuxtLink
+        to="/dashboard/users/participants"
+        class="py-2 px-4 rounded-t-lg"
+        :class="isActive('/dashboard/users/participants') ? 'bg-white text-[#2D5979]': 'bg-[#DEE6EF] text-[#2D5979]'"
+      >
         Participantes
       </NuxtLink>
     </div>
