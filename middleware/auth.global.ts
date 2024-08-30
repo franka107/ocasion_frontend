72ae1a559cdc71946fedd1746de72c8850465a69
JC Granda <juan.granda@instaleap.io>
2024-08-30 14:23:37 -0500

    feat: recovery pasword

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,11 +1,16 @@
-const PUBLIC_ROUTES = ['/auth/login', '/auth/register', '/auth/update-password']
+const PUBLIC_ROUTES = [
+  "/auth/login",
+  "/auth/register",
+  "/auth/update-password",
+];
 export default defineNuxtRouteMiddleware((to, from) => {
-    const { loggedIn } = useUserSession()
-    
-    if (!PUBLIC_ROUTES.includes(to.path) && !loggedIn.value){
-        return navigateTo("/auth/login")
-    } else if (PUBLIC_ROUTES.includes(to.path) && loggedIn.value) {
-        return navigateTo("/backoffice/organizations")
-    }
-    return
-  })
+  const { loggedIn } = useUserSession();
+
+  if (!PUBLIC_ROUTES.includes(to.path) && !loggedIn.value) {
+    return navigateTo("/auth/login");
+  } else if (PUBLIC_ROUTES.includes(to.path) && loggedIn.value) {
+    return navigateTo("/organizations");
+  }
+  return;
+});
+
--- pages/users/administrators.vue
+++ pages/users/administrators.vue
@@ -1,100 +1,107 @@
 <template>
-	<ContentLayout title="Usuarios" >
-		<CustomSimpleCard
-    title="Panel Super administrador"
-		class="mb-6"
-    sub-title="Gestiona eventos usuarios y reportes"
+  <ContentLayout title="Usuarios">
+    <CustomSimpleCard
+      title="Panel Super administrador"
+      class="mb-6"
+      sub-title="Gestiona eventos usuarios y reportes"
     />
 
-  <div class="w-full flex flex-col">
-    <div class="shadow-md rounded-lg px-6 bg-white flex-grow mb-auto">
-      <CustomTable
-        :data="adminsData"
-        :header="administratorsHeader"
-        @onSort="onSort"
-        @onSearch="onSearch"
-      >
-        <template #action-button>
-          <Button
-             @click="handleExport"
-             variant="default"
-             class="bg-white text-primary border border-[#052339] "
-          >
-          <CustomIcons name="Download" class="ml-auto" />
-            Exportar
-          </Button>
-          <Button
-            @click="() => { admsUserId = undefined; openModal = true; }"
-            variant="default"
-            >Agregar</Button
-          >
-        </template>
-        <template #type="{ row }">
-          <span class="whitespace-nowrap">{{  userType.get(row.type) || '' }}</span>
-        </template>
-        <template #actions="{ row }">
-          <div class="flex justify-center">
-            <DropdownMenu>
-              <DropdownMenuTrigger as-child>
-                <Button
-                  variant="ghost"
-                  size="sm"
-                  class="h-8 data-[state=open]:bg-accent"
+    <div class="w-full flex flex-col">
+      <div class="shadow-md rounded-lg px-6 bg-white flex-grow mb-auto">
+        <CustomTable
+          :data="adminsData"
+          :header="administratorsHeader"
+          @onSort="onSort"
+          @onSearch="onSearch"
+        >
+          <template #action-button>
+            <Button
+              variant="default"
+              href="http://localhost:4000/api/v1/user-management/export-users"
+              target="_blank"
+              as="a"
+              class="bg-white text-primary border border-[#052339]"
+            >
+              <CustomIcons name="Download" class="ml-auto" />
+              Exportar
+            </Button>
+            <Button
+              @click="
+                () => {
+                  admsUserId = undefined;
+                  openModal = true;
+                }
+              "
+              variant="default"
+              >Agregar</Button
+            >
+          </template>
+          <template #type="{ row }">
+            <span class="whitespace-nowrap">{{
+              userType.get(row.type) || ""
+            }}</span>
+          </template>
+          <template #actions="{ row }">
+            <div class="flex justify-center">
+              <DropdownMenu>
+                <DropdownMenuTrigger as-child>
+                  <Button
+                    variant="ghost"
+                    size="sm"
+                    class="h-8 data-[state=open]:bg-accent"
+                  >
+                    <CustomIcons name="VerticalDots" class="w-6 h-6" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  align="start"
+                  class="bg-primary text-white"
                 >
-                  <CustomIcons name="VerticalDots" class="w-6 h-6" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="start" class="bg-primary text-white">
-                <DropdownMenuItem
-                  @click="handleSuspend(row.id, row.fullName)"
-                  :disabled="row.status !== 'ACTIVE'"
-                >
-                  Suspender
-                  <CustomIcons name="Forbidden" class="ml-auto" />
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem
-                  @click="handleResetPassword(row.id)"
-                >
-                  Reenviar Correo 
-                  <CustomIcons name="Reload" class="ml-auto" />
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
+                  <DropdownMenuItem
+                    @click="handleSuspend(row.id, row.fullName)"
+                    :disabled="row.status !== 'ACTIVE'"
+                  >
+                    Suspender
+                    <CustomIcons name="Forbidden" class="ml-auto" />
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem @click="handleResetPassword(row.id)">
+                    Reenviar Correo
+                    <CustomIcons name="Reload" class="ml-auto" />
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
                   <DropdownMenuItem @click="handleUpdateForm(row)">
                     Actualizar datos
                     <CustomIcons name="ArrowLeft" class="ml-auto" />
                   </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        </template>
-        <template #status="{ row }">
-          <CustomChip
-            :text="row.status === 'ACTIVE' ? 'Activo' : 'Suspendido'"
-            :variant="row.status === 'ACTIVE' ? 'default' : 'destructive'"
-          ></CustomChip>
-        </template>
-      </CustomTable>
-      <!-- Fomulario -->
-      <SheetContent
-        class="flex flex-col h-full"
-        v-model:open="openModal"
-      >
-        <AdministratorsForm
-          :id="admsUserId"
-          :onSubmit="admsUserId !== undefined ? handleEdit : handleCreate"
-        />
-      </SheetContent>
-      <!-- Fomulario -->
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </template>
+          <template #status="{ row }">
+            <CustomChip
+              :text="row.status === 'ACTIVE' ? 'Activo' : 'Suspendido'"
+              :variant="row.status === 'ACTIVE' ? 'default' : 'destructive'"
+            ></CustomChip>
+          </template>
+        </CustomTable>
+        <!-- Fomulario -->
+        <SheetContent class="flex flex-col h-full" v-model:open="openModal">
+          <AdministratorsForm
+            :id="admsUserId"
+            :onSubmit="admsUserId !== undefined ? handleEdit : handleCreate"
+          />
+        </SheetContent>
+        <!-- Fomulario -->
+      </div>
+      <CustomPagination
+        class="mt-5 mb-[19px]"
+        :total="data.count"
+        :limit="data.limit"
+        v-model:page="page"
+      />
     </div>
-    <CustomPagination
-      class="mt-5 mb-[19px]"
-      :total="data.count"
-      :limit="data.limit"
-      v-model:page="page"
-    /> 
-  </div>
-	</ContentLayout>
+  </ContentLayout>
 </template>
 <script setup lang="ts">
 import AdministratorsForm from "~/components/users/administrators/AdministratorsForm.vue";
@@ -102,29 +109,39 @@ import CustomTable from "@/components/ui/custom-table/CustomTable.vue";
 import CustomChip from "@/components/ui/custom-chip/CustomChip.vue";
 import CustomIcons from "@/components/ui/custom-icons/CustomIcons.vue";
 import CustomPagination from "@/components/ui/custom-pagination/CustomPagination.vue";
-import { userType } from '~/constants/administrators';
+import { userType } from "~/constants/administrators";
 import type { IAdminsLItem } from "@/types/Administrators.ts";
 import { administratorsHeader } from "~/constants/administrators";
 import ContentLayout from "~/layouts/default/ContentLayout.vue";
 import CustomSimpleCard from "~/components/ui/custom-simple-card/CustomSimpleCard.vue";
 
-const route = useRoute()
-const {page, sortOptions, onSort, suspendUser,createUser, editUser, resetUser, getExportUser} = useAdmins();
+const route = useRoute();
+const {
+  page,
+  sortOptions,
+  onSort,
+  suspendUser,
+  createUser,
+  editUser,
+  resetUser,
+  getExportUser,
+} = useAdmins();
 const { openConfirmModal, updateConfirmModal } = useConfirmModal();
-const filterOptions = ref(`[{"field":"type","type":"not","value": "PARTICIPANT"}]`)
-const openModal = ref(false)
-const admsUserId = ref<number | undefined>(undefined)
+const filterOptions = ref(
+  `[{"field":"type","type":"not","value": "PARTICIPANT"}]`,
+);
+const openModal = ref(false);
+const admsUserId = ref<number | undefined>(undefined);
 const BASE_ADM_URL = "/user-management";
-const onSearch = (item: {[key: string]: string }) => {
-  console.log(item)
+const onSearch = (item: { [key: string]: string }) => {
+  console.log(item);
   const filters = [
-    { field:"type", type:"not", value:"PARTICIPANT" || ''},
-    { field: 'firstName', type: 'like', value: item.fullName || '' },
-    { field: 'status', type: 'equal', value: item.status || '' },
-
-  ]
-  filterOptions.value = JSON.stringify(filters)
-}
+    { field: "type", type: "not", value: "PARTICIPANT" || "" },
+    { field: "firstName", type: "like", value: item.fullName || "" },
+    { field: "status", type: "equal", value: item.status || "" },
+  ];
+  filterOptions.value = JSON.stringify(filters);
+};
 
 const { data, refresh }: any = await useAPI(
   `${BASE_ADM_URL}/find-users-paginated`,
@@ -138,16 +155,17 @@ const { data, refresh }: any = await useAPI(
   } as any,
 );
 
-const adminsData= computed(() => data.value.data.map((item: IAdminsLItem) => ({
+const adminsData = computed(() =>
+  data.value.data.map((item: IAdminsLItem) => ({
     fullName: `${item.firstName} ${item.lastName}`,
     document: `${item.documentType} - ${item.documentIdentifier}`,
     cellphone: item.phoneNumber,
-    organization: item.organizations.map(org => org.name).join(', '),
-    ...item
-  })))
+    organization: item.organizations.map((org) => org.name).join(", "),
+    ...item,
+  })),
+);
 
-
-  const handleSuspend = async (id: string, fullName: string) => {
+const handleSuspend = async (id: string, fullName: string) => {
   openConfirmModal({
     title: "Suspender usuario",
     message: `¿Estás seguro de suspender a ❝${fullName}❞?`,
@@ -163,7 +181,8 @@ const adminsData= computed(() => data.value.data.map((item: IAdminsLItem) => ({
       } else {
         updateConfirmModal({
           title: "Error al suspender",
-          message: "El usuario no se pudo suspender. \nTe recomendamos intentarlo nuevamente.",
+          message:
+            "El usuario no se pudo suspender. \nTe recomendamos intentarlo nuevamente.",
           type: "error",
         });
       }
@@ -177,11 +196,13 @@ const handleUpdateForm = async (user: any) => {
 };
 
 const handleCreate = async (values: any) => {
-  console.log("handleCreate ejecutado", values); 
-  openConfirmModal({title: 'Crear usuario', message: '¿Estás seguro de que deseas crear este usuario?',
+  console.log("handleCreate ejecutado", values);
+  openConfirmModal({
+    title: "Crear usuario",
+    message: "¿Estás seguro de que deseas crear este usuario?",
     callback: async () => {
-      const { status, error } : any = await createUser(values);
-      if (status.value === 'success') {
+      const { status, error }: any = await createUser(values);
+      if (status.value === "success") {
         openModal.value = false;
         refresh();
         updateConfirmModal({
@@ -190,7 +211,10 @@ const handleCreate = async (values: any) => {
           type: "success",
         });
       } else {
-        const eMsg = error.value.data?.errors?.[0].message || error.value.data.message || "El usuario no se pudo crear, intentalo más tarde";
+        const eMsg =
+          error.value.data?.errors?.[0].message ||
+          error.value.data.message ||
+          "El usuario no se pudo crear, intentalo más tarde";
         updateConfirmModal({
           title: "Error al crear usuario",
           message: eMsg,
@@ -204,9 +228,11 @@ const handleCreate = async (values: any) => {
 const handleEdit = async (values: any) => {
   console.log("handleEdit ejecutado", values);
   openConfirmModal({
-    title: 'Actualizar usuario', message: '¿Estás seguro de que deseas actualizar este usuario?', callback: async () => {
-      const { status, error } : any = await editUser(values);
-      if (status.value === 'success') {
+    title: "Actualizar usuario",
+    message: "¿Estás seguro de que deseas actualizar este usuario?",
+    callback: async () => {
+      const { status, error }: any = await editUser(values);
+      if (status.value === "success") {
         openModal.value = false;
         refresh();
         updateConfirmModal({
@@ -231,9 +257,11 @@ const handleEdit = async (values: any) => {
 
 const handleResetPassword = async (values: any) => {
   openConfirmModal({
-    title: 'Restablecer contraseña de usuario', message: '¿Estás seguro de que deseas restablecer contraseña de usuario?', callback: async () => {
-      const { status, error } : any = await resetUser(values);
-      if (status.value === 'success') {
+    title: "Restablecer contraseña de usuario",
+    message: "¿Estás seguro de que deseas restablecer contraseña de usuario?",
+    callback: async () => {
+      const { status, error }: any = await resetUser(values);
+      if (status.value === "success") {
         openModal.value = false;
         refresh();
         updateConfirmModal({
@@ -266,18 +294,20 @@ const handleExport = async () => {
         const url = window.URL.createObjectURL(new Blob([file]));
         const link = document.createElement("a");
         link.href = url;
-        link.setAttribute("download", "usuarios_exportados.xlsx");
+        link.setAttribute("download", "usuarios_exportados.csv");
         document.body.appendChild(link);
         link.click();
         link.remove();
-        
+
         updateConfirmModal({
           title: "Exportación exitosa",
           message: "Los usuarios han sido exportados exitosamente.",
           type: "success",
         });
       } else {
-        const eMsg = error.value?.data?.message || "Error al exportar usuarios. Inténtalo de nuevo más tarde.";
+        const eMsg =
+          error.value?.data?.message ||
+          "Error al exportar usuarios. Inténtalo de nuevo más tarde.";
         updateConfirmModal({
           title: "Error al exportar usuarios",
           message: eMsg,
