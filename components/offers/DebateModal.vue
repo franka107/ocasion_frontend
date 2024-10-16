95d1e9c3ca5c0aea906c780a4b889ddf0335b670
JC Granda <juan.granda@instaleap.io>
2024-10-16 14:36:15 -0500

    feat: discuss fix

--- components/offers/DebateModal.vue
+++ components/offers/DebateModal.vue
@@ -1,76 +1,76 @@
 <script setup lang="ts">
+import { toTypedSchema } from '@vee-validate/zod'
+import { useForm } from 'vee-validate'
+import { z } from 'zod'
 import {
   AlertDialog,
   AlertDialogContent,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-} from "@/components/ui/alert-dialog";
-import { toTypedSchema } from "@vee-validate/zod";
-import { useForm } from "vee-validate";
-import { z } from "zod";
+} from '@/components/ui/alert-dialog'
 
 const props = defineProps<{
-  id: string;
-  name: string;
-  appraisal: number;
-  modelValue: boolean;
-  refreshTable: () => void;
-}>();
-const { openConfirmModal, updateConfirmModal } = useConfirmModal();
-const { discussOffer } = useOfferAPI();
+  id: string
+  name: string
+  appraisal: number
+  modelValue: boolean
+  refreshTable: () => void
+}>()
+const { openConfirmModal, updateConfirmModal } = useConfirmModal()
+const { discussOffer } = useOfferAPI()
 const formSchema = toTypedSchema(
   z.object({
     counterProposalAmount: z
       .number()
-      .gte(1, { message: "La contrapropuesta es requerida." })
-      .lte(999999, { message: "El campo no puede exceder los 6 dígitos." }),
+      .gte(1, { message: 'La contrapropuesta es requerida.' })
+      .lte(999999, { message: 'El campo no puede exceder los 6 dígitos.' }),
   }),
-);
-const emit = defineEmits(["update:modelValue"]);
+)
+const emit = defineEmits(['update:modelValue'])
 const form = useForm({
   validationSchema: formSchema,
-});
+})
 const onSubmit = form.handleSubmit((values: any) => {
-  const { counterProposalAmount } = values;
-  handleSubmit({ offerId: props.id, counterProposalAmount });
-  console.log("counterProposalAmount", counterProposalAmount);
-});
+  const { counterProposalAmount } = values
+  handleSubmit({ offerId: props.id, counterProposalAmount })
+  console.log('counterProposalAmount', counterProposalAmount)
+})
 const handleSubmit = async (values: any) => {
   openConfirmModal({
-    title: "Enviar Oferta",
-    message: "¿Estás seguro de que deseas enviar esta Oferta?",
+    title: 'Enviar Oferta',
+    message: '¿Estás seguro de que deseas enviar este debate de oferta?',
     callback: async () => {
-      const { status, error }: any = await discussOffer(values);
-      if (status.value === "success") {
-        emit("update:modelValue", false);
-        props.refreshTable();
+      const { status, error }: any = await discussOffer(values)
+      if (status.value === 'success') {
+        emit('update:modelValue', false)
+        props.refreshTable()
         updateConfirmModal({
-          title: "Oferta enviada",
-          message: "La oferta ha sido enviada exitosamente",
-          type: "success",
-        });
+          title: 'Oferta enviada',
+          message: 'El debate de oferta ha sido enviada exitosamente',
+          type: 'success',
+        })
       } else {
         const eMsg =
           error.value.data?.errors?.[0].message ||
           error.value.data.message ||
-          "La oferta no se pudo enviar, intentalo más tarde";
+          'La oferta no se pudo enviar, intentalo más tarde'
         updateConfirmModal({
-          title: "Error al crear Oferta",
+          title: 'Error al crear Oferta',
           message: eMsg,
-          type: "error",
-        });
+          type: 'error',
+        })
       }
     },
-  });
-};
+  })
+}
 </script>
 
 <template>
   <AlertDialog
     :open="modelValue"
-    @update:open="(event) => emit('update:modelValue', event)"
     class="z-[30]"
+    @update:open="(event) => emit('update:modelValue', event)"
   >
     <AlertDialogContent class="z-[98] max-w-[600px] px-0">
       <form class="flex flex-col gap-10 flex-grow" @submit="onSubmit">
@@ -87,7 +87,7 @@ const handleSubmit = async (values: any) => {
             label="Nombre del evento"
             :model-value="props.name"
             :disabled="true"
-            labelOffset
+            label-offset
           />
           <CustomInput
             class="h-14 w-full"
@@ -95,7 +95,7 @@ const handleSubmit = async (values: any) => {
             label="Tasación"
             :model-value="props.appraisal"
             :disabled="true"
-            labelOffset
+            label-offset
           />
           <FormField v-slot="{ componentField }" name="counterProposalAmount">
             <FormItem>
@@ -105,7 +105,7 @@ const handleSubmit = async (values: any) => {
                   type="number"
                   label="Tasación"
                   v-bind="componentField"
-                  labelOffset
+                  label-offset
                 />
               </FormControl>
               <FormMessage />
@@ -115,14 +115,14 @@ const handleSubmit = async (values: any) => {
 
         <AlertDialogFooter class="px-6">
           <Button
-            @click="
-              () => {
-                emit('update:modelValue', false);
-              }
-            "
             type="button"
             size="xl"
             class="text-[16px] font-[600] bg-white text-primary border border-primary hover:bg-accent mt-[16px]"
+            @click="
+              () => {
+                emit('update:modelValue', false)
+              }
+            "
             >Cancelar</Button
           >
           <Button
@@ -137,4 +137,3 @@ const handleSubmit = async (values: any) => {
     </AlertDialogContent>
   </AlertDialog>
 </template>
-
