5b15e3f0b36b6f3879ea015162efccc0625f3991
JC Granda <juan.granda@instaleap.io>
2024-11-09 01:30:57 -0500

    files support

--- composables/usePayment.ts
+++ composables/usePayment.ts
@@ -1,38 +1,39 @@
-import { useAPI } from "./useAPI";
+import { useAPI } from './useAPI'
 
-const BASE_PAY_URL = "/payment-management";
+const BASE_PAY_URL = '/payment-management'
 
-export function usePaymentAPI(){
-    const page = ref(1);
-    const sortOptions = ref("[]");
-    const onSort = (sortObject: { [key: string]: string }[]) => {
-        sortOptions.value = JSON.stringify(sortObject);
-      };
-    const observePayment = async (values: {paymentId: string , reason: string}) => {
-        const { status, error }: any = await useAPI(
-          `${BASE_PAY_URL}/observe-payment`,
-          {
-            method: "POST",
-            body: values,
-          } as any
-        );
-      
-        return { status, error };
-    };
+export function usePaymentAPI() {
+  const page = ref(1)
+  const sortOptions = ref('[]')
+  const onSort = (sortObject: { [key: string]: string }[]) => {
+    sortOptions.value = JSON.stringify(sortObject)
+  }
+  const observePayment = async (values: {
+    paymentId: string
+    reason: string
+  }) => {
+    const { status, error }: any = await useAPI(
+      `${BASE_PAY_URL}/observe-payment`,
+      {
+        method: 'POST',
+        body: values,
+      } as any,
+    )
 
-    const confirmPayment = async (values: { paymentId: string }) => {
-        const { status, error }: any = await useAPI(
-          `${BASE_PAY_URL}/confirm-payment`,
-          {
-            method: "POST",
-            body: values,
-          } as any
-        );
-      
-        return { status, error };
-    };
-    
+    return { status, error }
+  }
 
-      return { confirmPayment, observePayment, sortOptions, page, onSort }
+  const confirmPayment = async (values: { paymentId: string }) => {
+    const { status, error }: any = await useAPI(
+      `${BASE_PAY_URL}/confirm-payment`,
+      {
+        method: 'POST',
+        body: values,
+      } as any,
+    )
+
+    return { status, error }
+  }
+
+  return { confirmPayment, observePayment, sortOptions, page, onSort }
 }
-
--- pages/dashboard/platform/payments/index.vue
+++ pages/dashboard/platform/payments/index.vue
@@ -2,5 +2,5 @@
   <PaymentPage :organization-id="null" type="platform" />
 </template>
 <script setup lang="ts">
-import PaymentPage from "~/components/payments/PaymentPage.vue";
+import PaymentPage from '~/components/payments/PaymentPage.vue'
 </script>
