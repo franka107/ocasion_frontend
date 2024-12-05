df750f04583b299321b13cbc584ce1e7c02275ee
JC Granda <juan.granda@instaleap.io>
2024-12-05 17:47:07 -0500

    feat: compost support

--- constants/events.ts
+++ constants/events.ts
@@ -89,6 +89,15 @@ export const eventStatusCheckPosition = (
 }
 
 export const eventTimes = new Map<string, string>([
+  // Temporal
+  ['1', '1:00'],
+  ['2', '2:00'],
+  ['3', '3:00'],
+  ['4', '4:00'],
+  ['5', '5:00'],
+  ['6', '6:00'],
+  ['7', '7:00'],
+  // ----
   ['8', '8:00'],
   ['9', '9:00'],
   ['10', '10:00'],
@@ -99,6 +108,15 @@ export const eventTimes = new Map<string, string>([
   ['15', '15:00'],
   ['16', '16:00'],
   ['17', '17:00'],
+  // Temporal
+  ['18', '18:00'],
+  ['19', '19:00'],
+  ['20', '20:00'],
+  ['21', '21:00'],
+  ['22', '22:00'],
+  ['23', '23:00'],
+  ['24', '24:00'],
+  // ----
 ])
 export const goodType = new Map<string, string>([
   ['REAL_ESTATE', 'Inmuebles'],
