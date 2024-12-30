67411ad4a6132307ef623f4dea0f429172f7d06b
Oliver Joe <olivergrandaramos@gmail.com>
2024-12-30 16:42:55 -0500

    updateevents

--- layouts/default/DetailLayout.vue
+++ layouts/default/DetailLayout.vue
@@ -20,7 +20,7 @@
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
-            class="w-5 h-6 transform transition-transform duration-200"
+            class="w-5 h-5 transform transition-transform duration-200"
             :class="showContent ? 'rotate-180' : 'rotate-0'"
           >
             <path
