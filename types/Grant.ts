475ba8fc38e70c926bcd05ea02375b4ad7287d3a
JC Granda <juan.granda@instaleap.io>
2025-02-17 22:07:40 -0500

    feat: clean migrations

--- types/Grant.ts
+++ types/Grant.ts
@@ -1,5 +1,6 @@
 export enum GrantId {
   /* Platform */
+  PlatformUsersCanReactivateParticipant = 'PLATFORM_USERS_CAN_REACTIVATE_PARTICIPANT',
   PlatformKpisManagement = 'PLATFORM_KPIS_MANAGEMENT',
   PlatformUsersManagement = 'PLATFORM_USERS_MANAGEMENT',
   PlatformUsersCanCreate = 'PLATFORM_USERS_CAN_CREATE',
